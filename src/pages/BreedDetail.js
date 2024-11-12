d953318569657d4cff64fce7228843f4a9913984
Ian Kabaka <iankabaka1@gmail.com>
2024-11-12 20:56:52 +0300

    testing with static data

--- src/pages/BreedDetail.js
+++ src/pages/BreedDetail.js
@@ -1,9 +1,16 @@
 import { useParams } from "react-router-dom";
 function BreedDetail() {
   const { id } = useParams();
+  const breedData = {
+    1: { name: "rotweller", description: "angry and mad" },
+    2: { name: "rhodesian ridgeback", description: "known for hunting lions" },
+  };
+  const breed = breedData[id];
   return (
     <div>
-      <p>Breed Details</p>
+      <h1>New Breed info!</h1>
+      <p>{breed.name}</p>
+      <p>{breed.description}</p>
       <p>View breeds id:{id}</p>
     </div>
   );
