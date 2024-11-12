80ed3280402b12863e565bd85dc3d5e447dc3e08
Ian Kabaka <iankabaka1@gmail.com>
2024-11-12 13:10:20 +0300

    render child routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-
+import { Outlet } from "react-router-dom";
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
