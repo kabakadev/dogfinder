1b4a0fe4d98e9df8a6765ef5f21e7a34c9bbbcb0
Ian Kabaka <iankabaka1@gmail.com>
2024-11-12 21:52:59 +0300

    fetching data from the json server

--- src/pages/BreedDetail.js
+++ src/pages/BreedDetail.js
@@ -1,10 +1,17 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 function BreedDetail() {
+  const baseUrl = "http://localhost:3000/breeds";
   const { id } = useParams();
-  const breedData = {
-    1: { name: "rotweller", description: "angry and mad" },
-    2: { name: "rhodesian ridgeback", description: "known for hunting lions" },
-  };
+  // const breedData = {
+  //   1: { name: "rotweller", description: "angry and mad" },
+  //   2: { name: "rhodesian ridgeback", description: "known for hunting lions" },
+  // };
+  useEffect(() => {
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
   const breed = breedData[id];
   return (
     <div>
