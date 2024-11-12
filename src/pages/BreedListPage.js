33255b4075a647b013c9bd8e8fe6a93056007dd1
Ian Kabaka <iankabaka1@gmail.com>
2024-11-12 23:29:28 +0300

    maps over the breed data and links to the specific breed detail

--- src/pages/BreedListPage.js
+++ src/pages/BreedListPage.js
@@ -1,14 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function BreedListPage() {
   const baseUrl = "http://localhost:3000/breeds";
+  const [breeds, setBreeds] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch(baseUrl)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setBreeds(data);
+        setLoading(false);
       });
   }, []);
-  return <div>BreedListPage</div>;
+  if (loading) {
+    return <p>The data is still loading, please wait a moment.</p>;
+  }
+  return (
+    <div>
+      <h1>Available Breeds</h1>
+      <ul>
+        {breeds.map((breed) => {
+          <li key={breed.id}>
+            <Link to={`/breed/${breed.id}`}>{breed.breed}</Link>{" "}
+          </li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 export default BreedListPage;
