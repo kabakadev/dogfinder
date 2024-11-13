9c4f03f4a9e10d961fee3f17e55e10d994c4dd24
Ian Kabaka <iankabaka1@gmail.com>
2024-11-13 07:19:56 +0300

    posting the data to the server

--- src/pages/AddBreed.js
+++ src/pages/AddBreed.js
@@ -1,31 +1,95 @@
+import { useState } from "react";
+
 function AddBreed() {
+  const baseUrl = "http://localhost:3000/breeds";
+  const [breed, setBreed] = useState("");
+  const [description, setDescription] = useState("");
+  const [color, setColor] = useState("");
+  const [size, setSize] = useState("");
+  const [lifeSpan, setLifeSpan] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  function handleSubmit(e) {
+    e.preventDefaut();
+    fetch(baseUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        breed,
+        description,
+        color,
+        size,
+        lifeSpan,
+        image: imageUrl,
+      })
+        .then((res) => res.json())
+        .then((data) =>
+          console.log(`Data:${data} has been posted successfully! `)
+        ),
+    });
+  }
   return (
-   
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         Breed:
-        <input type="text" name="name" value={} onChange={} required/>
+        <input
+          type="text"
+          name="name"
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+          required
+        />
       </label>
       <label>
         Description:
-        <input type="text" name="name" value={} onChange={} required/>
+        <input
+          type="text"
+          name="name"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
       </label>
       <label>
         color:
-        <input type="text" name="name" value={} onChange={} required/>
+        <input
+          type="text"
+          name="name"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          required
+        />
       </label>
       <label>
         size:
-        <input type="text" name="name" value={} onChange={} required/>
+        <input
+          type="text"
+          name="name"
+          value={size}
+          onChange={(e) => setSize(e.target.value)}
+          required
+        />
       </label>
       <label>
         lifespan:
-        <input type="text" name="name" value={} onChange={} required/>
+        <input
+          type="text"
+          name="name"
+          value={lifeSpan}
+          onChange={(e) => setLifeSpan(e.target.value)}
+          required
+        />
       </label>
       <label>
         image:
-        <input type="text" name="name" value={} onChange={} required/>
+        <input
+          type="text"
+          name="name"
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+          required
+        />
       </label>
+      <button type="submit">Add breed </button>
     </form>
   );
 }
