7e4aa469a8f0a590c961790f9c372ff35f2ec1ef
Ian Kabaka <iankabaka1@gmail.com>
2024-11-13 21:32:37 +0300

    adds more content to the Home landing page and styles it further

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 function Home() {
   return (
-    <div>
-      <p className="text-blue-400">
-        This is the breed finder for your favorite dogs, feel free to browse
-        around!
-      </p>
+    <div className="container mx-auto px-4 py-8">
+      <section className="mb-8">
+        <h1 className="text-4xl font-bold text-gray-800 mb-2">
+          Explore a variety of Dog Breeds!
+        </h1>
+        <p className="text-lg text-gray-500">
+          Have a look at the wide selection of the most popular dog breeds. Feel
+          free to also add your own Dog breed here.
+        </p>
+      </section>
     </div>
   );
 }
