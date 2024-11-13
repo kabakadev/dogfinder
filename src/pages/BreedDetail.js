a050f00abcbe68efc942b0d01efea47a82ae289e
Ian Kabaka <iankabaka1@gmail.com>
2024-11-13 18:40:13 +0300

    adds Card to breedDetail component

--- src/pages/BreedDetail.js
+++ src/pages/BreedDetail.js
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import BreedCard from "../components/BreedCard";
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 function BreedDetail() {
   const [breedData, setBreedData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -23,10 +31,27 @@ function BreedDetail() {
       <h1>New Breed info!</h1>
       <h2>{breedData.name}</h2>
       <p>{breedData.description}</p>
-      <li>
+      <p>Color: {breedData.color}</p>
+      <p>Size: {breedData.size}</p>
+      <Card
+        sx={{
+          maxWidth: 200,
+          margin: "1.25rem",
+          boxShadow: 3,
+          borderRadius: "8px",
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="140"
+          image={breedData.image}
+          alt="an example image"
+        />
+      </Card>
+      {/* <li>
         <img src={breedData.image} alt="dog breed" />
         <p> {breedData.color}</p>
-      </li>
+      </li> */}
     </div>
   );
 }
