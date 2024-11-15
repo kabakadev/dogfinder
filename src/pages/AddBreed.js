a350380b978dd3d738a40ebea40a08b92f869bc1
Ian Kabaka <iankabaka1@gmail.com>
2024-11-15 22:45:45 +0300

    styles the form to be a bit modern using tailwind recommended styles

--- src/pages/AddBreed.js
+++ src/pages/AddBreed.js
@@ -8,8 +8,7 @@ function AddBreed() {
   const [size, setSize] = useState("");
   const [lifeSpan, setLifeSpan] = useState("");
   const [imageUrl, setImageUrl] = useState("");
-  const styleClasses =
-    "mt-1 block w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400";
+
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -38,85 +37,123 @@ function AddBreed() {
       });
   }
   return (
-    <div className="bg-gray-200 h-screen p-4 flex justify-center items-start">
-      <div className="max-w-lg">
-        {" "}
-        <form onSubmit={handleSubmit}>
-          <label className="block mb-4">
-            Breed:
-            <input
-              type="text"
-              name="name"
-              value={breed}
-              onChange={(e) => setBreed(e.target.value)}
-              required
-              className={styleClasses}
-            />
-          </label>
-          <label className="block mb-4">
-            Description:
-            <input
-              type="text"
-              name="name"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              required
-              className={styleClasses}
-            />
-          </label>
-          <label className="block mb-4">
-            Color:
-            <input
-              type="text"
-              name="name"
-              value={color}
-              onChange={(e) => setColor(e.target.value)}
-              required
-              className={styleClasses}
-            />
-          </label>
-          <label className="block mb-4">
-            Size:
-            <input
-              type="text"
-              name="name"
-              value={size}
-              onChange={(e) => setSize(e.target.value)}
-              required
-              className={styleClasses}
-            />
-          </label>
-          <label className="block mb-4">
-            Lifespan:
-            <input
-              type="text"
-              name="name"
-              value={lifeSpan}
-              onChange={(e) => setLifeSpan(e.target.value)}
-              required
-              className={styleClasses}
-            />
-          </label>
-          <label className="block mb-4">
-            ImageUrl:
-            <input
-              type="text"
-              name="name"
-              value={imageUrl}
-              onChange={(e) => setImageUrl(e.target.value)}
-              required
-              className={styleClasses}
-            />
-          </label>
-          <button
-            type="submit"
-            className="mt-4 px-6 mx-auto flex align-center  bg-green-700 text-white font-bold rounded-md hover:bg-green-800 transistion"
-          >
-            Add breed{" "}
-          </button>
-        </form>
+    <form className="max-w-md mx-auto mt-4" onSubmit={handleSubmit}>
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="text"
+          name="breeds"
+          id="breed"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-blue dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          value={breed}
+          onChange={(e) => setBreed(e.target.value)}
+          required
+        />
+        <label
+          htmlFor="breed"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Breed
+        </label>
       </div>
-    </div>
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="text"
+          name="description"
+          id="description"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-blue dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          required
+        />
+        <label
+          htmlFor="floating_password"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Description
+        </label>
+      </div>
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="text"
+          name="color"
+          id="color"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-blue dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          required
+        />
+        <label
+          htmlFor="color"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Color
+        </label>
+      </div>
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="text"
+          name="size"
+          id="size"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-blue dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          value={size}
+          onChange={(e) => setSize(e.target.value)}
+          required
+        />
+        <label
+          htmlFor="size"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Size
+        </label>
+      </div>
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="text"
+          name="color"
+          id="lifespan"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-blue dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          value={lifeSpan}
+          onChange={(e) => setLifeSpan(e.target.value)}
+          required
+        />
+        <label
+          htmlFor="lifespan"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          lifespan
+        </label>
+      </div>
+      <div className="relative z-0 w-full mb-5 group">
+        <input
+          type="text"
+          name="image"
+          id="image"
+          className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-blue dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+          placeholder=" "
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+          required
+        />
+        <label
+          htmlFor="color"
+          className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+        >
+          Image url
+        </label>
+      </div>
+
+      <button
+        type="submit"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Submit
+      </button>
+    </form>
   );
 }
 export default AddBreed;
